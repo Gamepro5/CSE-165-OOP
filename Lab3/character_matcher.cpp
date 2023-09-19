68f693ced452734a340c21f342db07922e3ce25d
Gamepro5 <gamepro543@gmail.com>
2023-09-18 22:50:44 -0700

    work

--- Lab3/character_matcher.cpp
+++ Lab3/character_matcher.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 int main() {
-  
+  vector<string> list;
+  while (true) {
+    string input;
+    cin >> input;
+    if (input == "quit") {
+      return 0;
+    }
+    if (input.length() > 1) {
+      list.push_back(input);
+    } else {
+      for (int i=0;i<list.size();i++) {
+        if ((list[i][0]) == input[0]) {
+          cout << list.at(i) << endl;
+        }
+      }
+      return 0;
+    }
+
+  }
   
   return 0;
 }
