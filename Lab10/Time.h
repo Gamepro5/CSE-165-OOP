4f630aa7d94e8267d22e21f254d794b36e9aaf7a
Gamepro5 <gamepro543@gmail.com>
2023-11-15 12:55:20 -0800

    lab 10 done

---
+++ Lab10/Time.h
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Time {
+protected:
+public:
+    int seconds;
+    int minutes;
+    int hours;
+    Time() {
+        hours = 0;
+        minutes = 0;
+        seconds = 0;
+    }
+    Time(int _hours, int _minutes, int _seconds) {
+        hours = _hours;
+        minutes = _minutes;
+        seconds = _seconds;
+    }
+    void print() {
+        std::cout << hours << " hours, " << minutes << " minutes, " << seconds << " seconds" << std::endl;
+    }
+    Time operator+(Time const& other) {
+        Time res;
+        res.seconds += seconds + other.seconds;
+        if (res.seconds >= 60) {
+            res.minutes += int(res.seconds/60);
+            res.seconds = res.seconds % 60;
+        }
+        res.minutes += minutes + other.minutes;
+        if (res.minutes >= 60) {
+            res.hours += int(res.minutes/60);
+            res.minutes = res.minutes % 60;
+        }
+        res.hours += hours + other.hours;
+        res.hours = res.hours % 24;
+        
+        return res;
+    }
+    Time operator++() {
+        hours++;
+        hours = hours % 24;
+        return *this;
+    }
+    Time operator++(int) {
+        Time temp = *this;
+        ++hours;
+        return temp;
+    }
+};
